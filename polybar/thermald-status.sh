fc917df7f89b7a58ab27be14e70db4c271e8bebf
Marko Trickovic <marko@trickovic.dev>
2026-07-30 16:10:19 +0200

    chore: remove X11/i3 stack for Wayland/Hyprland

--- polybar/thermald-status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if systemctl is-active --quiet thermald; then
-    TEMP=$(cat /sys/class/thermal/thermal_zone2/temp)
-    TEMP_C=$((TEMP / 1000))
-    echo " $TEMP_C°C"
-else
-    echo " off"
-fi
